0eda509fd7af20ca4cea0aa5485eda08167be503
Filipe Affonso <161388271+filiwili@users.noreply.github.com>
2025-06-23 15:21:35 -0300

    Delete PWEBNode/PwebNode directory

--- PWEBNode/PwebNode/Exercicios/EXERCICIOS ANTERIORES/exercicio4sincrono.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');   // carregando módulo filesystem
-const data = fs.readFileSync('file.txt');
-// a execucao é bloqueada aqui até o arquivo ser lido
-console.log(data.toString());
--- PWEBNode/PwebNode/Exercicios/app/routes/adicionar_usuario.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app) {
-    app.get('/formulario_adicionar_usuario', function(req,res){
-        res.render("admin/adicionar_usuario");
-    });
-}
